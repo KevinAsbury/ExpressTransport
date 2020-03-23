5929d0c85c222a2dcec5c6933383f1f07fa8be87
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-03-23 13:57:39 -0400

    auth decorators

--- app/routes.py
+++ app/routes.py
@@ -18,33 +18,41 @@ def index():
     return 'Hello World!'
 
 @app.route('/deliveries', methods=['GET'])
+@requires_auth('get:deliveries')
 def get_vehicles():
     return 'To be implemented'
 
 @app.route('/deliveries', methods=['POST'])
+@requires_auth('post:deliveries')
 def post_vehicle():
     return 'To be implemented'
 
 @app.route('/deliveries/<int:id>', methods=['PATCH'])
+@requires_auth('patch:deliveries')
 def update_vehicle(id):
     return 'To be implemented'
 
 @app.route('/deliveries/<int:id>', methods=['DELETE'])
+@requires_auth('delete:deliveries')
 def delete_vehicle(id):
     return 'To be implemented'
 
 @app.route('/drivers', methods=['GET'])
+@requires_auth('get:drivers')
 def get_drivers():
     return 'To be implemented'
 
 @app.route('/drivers', methods=['POST'])
+@requires_auth('post:drivers')
 def post_driver():
     return 'To be implemented'
 
 @app.route('/drivers/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drivers')
 def update_driver(id):
     return 'To be implemented'
 
 @app.route('/drivers/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drivers')
 def delete_driver(id):
     return 'To be implemented'
