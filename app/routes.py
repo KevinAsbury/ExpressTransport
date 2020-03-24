e3c4463ccc194a53d77ca4de81c4738cd27b8855
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-03-24 11:42:50 -0400

    endpoint testing

--- app/routes.py
+++ app/routes.py
@@ -20,43 +20,43 @@ def index():
 
 @app.route('/deliveries', methods=['GET'])
 @requires_auth('get:deliveries')
-def get_vehicles():
+def get_deliveries(jwt):
     return 'To be implemented'
 
 @app.route('/deliveries', methods=['POST'])
 @requires_auth('post:deliveries')
-def post_vehicle():
+def post_deliveries(jwt):
     return 'To be implemented'
 
 @app.route('/deliveries/<int:id>', methods=['PATCH'])
 @requires_auth('patch:deliveries')
-def update_vehicle(id):
-    return 'To be implemented'
+def update_deliveries(jwt, id):
+    return f'To be implemented: id {id}'
 
 @app.route('/deliveries/<int:id>', methods=['DELETE'])
 @requires_auth('delete:deliveries')
-def delete_vehicle(id):
-    return 'To be implemented'
+def delete_deliveries(jwt, id):
+    return f'To be implemented: id {id}'
 
 @app.route('/drivers', methods=['GET'])
 @requires_auth('get:drivers')
-def get_drivers():
+def get_drivers(jwt):
     return 'To be implemented'
 
-@app.route('/drivers', methods=['POST'])
-@requires_auth('post:drivers')
-def post_driver():
-    return 'To be implemented'
+# @app.route('/drivers', methods=['POST'])
+# @requires_auth('post:drivers')
+# def post_driver(jwt):
+#     return 'To be implemented'
 
-@app.route('/drivers/<int:id>', methods=['PATCH'])
-@requires_auth('patch:drivers')
-def update_driver(id):
-    return 'To be implemented'
+# @app.route('/drivers/<int:id>', methods=['PATCH'])
+# @requires_auth('patch:drivers')
+# def update_driver(jwt, id):
+#     return 'To be implemented'
 
-@app.route('/drivers/<int:id>', methods=['DELETE'])
-@requires_auth('delete:drivers')
-def delete_driver(id):
-    return 'To be implemented'
+# @app.route('/drivers/<int:id>', methods=['DELETE'])
+# @requires_auth('delete:drivers')
+# def delete_driver(jwt, id):
+#     return 'To be implemented'
 
 
 ## Error Handling
