a0bfa7e3d478e665660ae4fb4d3495b7148eab1d
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-03-24 15:20:22 -0400

    Environment Variables

--- app/auth.py
+++ app/auth.py
@@ -3,11 +3,12 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 
-AUTH0_DOMAIN = 'ka-dev.auth0.com'
+AUTH0_DOMAIN = os.environ.get('DOMAIN')
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'myexpressway'
+API_AUDIENCE = os.environ.get('AUDIENCE')
 
 ## AuthError Exception
 '''
--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 export FLASK_APP=app
+export AUDIENCE=myexpressway
+export DOMAIN=ka-dev.auth0.com
